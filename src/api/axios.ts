33f5029516ba42cf05b2dc9c5d1bb9f09b76b59d
Shivam Yadav <sy7176442@gmail.com>
2025-05-30 15:08:15 +0530

    env file

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true, // send cookies with every request
   headers: {
     "Content-Type": "application/json",
